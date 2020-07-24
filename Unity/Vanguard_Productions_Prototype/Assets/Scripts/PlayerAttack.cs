b8c9112758e0dd14936fb3ddbcbf3f4286ab5b43
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-07-24 23:51:33 +1000

    Updated Animations + Animation Controller + Masks + Objects
    Added more animations for female w/ sword.
    Added attacking layer.
    Added avatar mask to attacking layer. (only moves the torso.

---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/PlayerAttack.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour
+{
+    public animationScript playerAnim;
+
+    public bool attack;
+
+    private void Awake()
+    {
+        playerAnim = GetComponentInChildren<animationScript>();
+        attack = true;
+    }
+
+    private void Update()
+    {
+        ComboAttacks();
+    }
+
+    void ComboAttacks()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if(attack)
+            {
+                playerAnim.heavyAttack();
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            playerAnim.lightAttack();
+        }
+    }
+}
--- Unity/Vanguard_Productions_Prototype/Assets/Scripts/animationScript.cs
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/animationScript.cs
@@ -11,17 +11,16 @@ public class animationScript : MonoBehaviour
     public float InputX;
     public float InputZ;
 
-    //  distance to ground
-    float distanceToGround = 12.5f;
+    private bool stopLightAttack;
 
-    private Vector3 characterOrientation;
+    //private Vector3 characterOrientation;
 
     bool correctRotation = false;
 
     private void Start()
     {
         anim = GetComponent<Animator>();
-        characterOrientation = transform.parent.eulerAngles;
+        //characterOrientation = transform.parent.eulerAngles;
     }
 
     private void Update()
@@ -42,90 +41,44 @@ public class animationScript : MonoBehaviour
             // ====== side scroller movement ========
 
             // ====== Character Orientation =========
-            
+
             //update character orientation
             if (Input.GetKeyDown(KeyCode.A))
             {
-                transform.eulerAngles = new Vector3(0,180,0);
+                transform.eulerAngles = new Vector3(0, 180, 0);
                 //characterOrientation.y += 0.1f;
             }
             if (Input.GetKeyDown(KeyCode.D))
             {
-                transform.eulerAngles = new Vector3(0,0,0);
+                transform.eulerAngles = new Vector3(0, 0, 0);
             }
             else
             {
                 //correctRotation = true;
             }
-
-            // update orientation
-            transform.parent.eulerAngles = characterOrientation;
-
-            // ====== Character Orientation =========
-
-
-            if (correctRotation)
-            {
-                Vector3 finalPoint = new Vector3(0, 0, 0);
-                if(Vector3.Distance(transform.eulerAngles, finalPoint) > 0.01f)
-                {
-                    transform.eulerAngles = Vector3.Lerp(transform.rotation.eulerAngles, finalPoint, Time.deltaTime);
-                }
-                else
-                {
-                    transform.eulerAngles = finalPoint;
-                    correctRotation = false;
-                }
-            }
-
-            //InputZ = Input.GetAxis("Vertical"); //UP and DOWN arrow key
-            //InputX = Input.GetAxis("Horizontal"); //LEFT and RIGHT arrow key
-
-            ////if(Input.GetKeyDown(KeyCode.A))
-            ////{
-            ////    InputZ -= 0.1f;
-            ////}
-            ////if(Input.GetKeyDown(KeyCode.D))
-            ////{
-            ////    InputZ += 0.001f;
-            ////}
-            ////else
-            ////{
-
-            ////}
-
-            ////  Pass values through animator
-            //anim.SetFloat("InputZ", InputZ);
-            //anim.SetFloat("InputX", InputX);
-
-            //Debug.Log("InputZ: " + InputZ);
-            //Debug.Log("InputX: " + InputX);
-
-            ////  if player presses space bar
-            //if (Input.GetButtonDown("Jump"))
-            //{
-            //    anim.SetTrigger("isJump");
-            //}
         }
+    }
 
-        //Only for Open-World Sim
-        #region Free Fall Anim
+    // Animations For Attack
+    public void lightAttack()
+    {
+        anim.SetTrigger("lightAttack");
+    }
 
-        ////  if player is above ground by a certain distance, play freefall anim
-        //RaycastHit rayCast;
-        //if (Physics.Raycast(transform.position, Vector3.down, out rayCast, 0.10f))
-        //{
-        //    //if collision
-        //    //set free fall to false
-        //    anim.SetBool("isFalling", false);
-        //}
-        //else
-        //{
-        //    //if no collision
-        //    //set free fall to true
-        //    anim.SetBool("isFalling", true);
-        //}
+    public void heavyAttack()
+    {
+        anim.SetTrigger("heavyAttack");
+    }
 
-        #endregion
+    public void stopMovement()
+    {
+        // don't allow player to attack again
+        FindObjectOfType<PlayerAttack>().attack = false;
+    }
+
+    public void allowMovement()
+    {
+        // allow player to attack again
+        FindObjectOfType<PlayerAttack>().attack = true;
     }
 }
