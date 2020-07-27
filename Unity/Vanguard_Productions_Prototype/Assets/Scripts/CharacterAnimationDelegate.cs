584f5196f4b8731adc29505dd4065af339f4d3f2
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-07-27 16:31:09 +1000

    Saving Changes
    Saving Changes.

---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/CharacterAnimationDelegate.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationDelegate : MonoBehaviour
+{
+
+    public GameObject beg_Sword_Attack_Point, begToMid_Sword_Attack_Point, MidToEnd_Sword_Attack_Point, End_Sword_Attack_Point;
+
+    void beg_Sword_Attack_On()
+    {
+        beg_Sword_Attack_Point.SetActive(true);
+    }
+
+    void beg_Sword_Attack_Off()
+    {
+        if(beg_Sword_Attack_Point.activeInHierarchy)
+        {
+            beg_Sword_Attack_Point.SetActive(false);
+        }
+    }
+
+    void begToMid_Sword_Attack_On()
+    {
+        begToMid_Sword_Attack_Point.SetActive(true);
+    }
+
+    void begToMid_Sword_Attack_Off()
+    {
+        if(begToMid_Sword_Attack_Point.activeInHierarchy)
+        {
+            begToMid_Sword_Attack_Point.SetActive(false);
+        }
+    }
+
+    void MidToEnd_Sword_Attack_On()
+    {
+        MidToEnd_Sword_Attack_Point.SetActive(true);
+    }
+
+    void MidToEnd_Sword_Attack_Off()
+    {
+        if(MidToEnd_Sword_Attack_Point.activeInHierarchy)
+        {
+            MidToEnd_Sword_Attack_Point.SetActive(false);
+        }
+    }
+
+    void End_Sword_Attack_On()
+    {
+        End_Sword_Attack_Point.SetActive(true);
+    }
+
+    void End_Sword_Attack_Off()
+    {
+        if(End_Sword_Attack_Point.activeInHierarchy)
+        {
+            End_Sword_Attack_Point.SetActive(false);
+        }
+    }
+
+    void Tag_Beg_Sword_Point()
+    {
+        beg_Sword_Attack_Point.tag = "beg_Point";
+    }
+}
---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/HealthScript.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthScript : MonoBehaviour
+{
+    public float health = 100f;
+
+    private animationScript animationScript;
+    private EnemyMovement enemyMovement;
+
+    private bool characterDied;
+
+    public bool is_Player;
+
+    void Awake()
+    {
+        animationScript = GetComponentInChildren<animationScript>();
+    }
+
+    public void ApplyDamage(float damage, bool knockDown)
+    {
+        if(characterDied)
+        {
+            return;
+        }
+
+        health -= damage;
+
+        if(health <= 0f)
+        {
+            animationScript.Death();
+            characterDied = true;
+
+            if(is_Player)
+            {
+                return;
+            }
+
+            if(!is_Player)
+            {
+                if(knockDown)
+                {
+                    if(Random.Range(0,2) > 0)
+                    {
+                        //animationScript.knockDown();
+                    }
+                }
+                else
+                {
+                    if(Random.Range(0,3) > 1)
+                    {
+                        animationScript.Hit();
+                    }
+                }
+            }
+        }
+    }
+    
+}
