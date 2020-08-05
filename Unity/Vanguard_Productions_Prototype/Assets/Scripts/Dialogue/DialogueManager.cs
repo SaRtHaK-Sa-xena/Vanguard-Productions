0ba66720c89b3b19e482c7a558ce7ff956b2dad4
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-08-05 12:22:05 +1000

    Update Dialogue, Camera
    Added Dialogue, Working on creating a better dialogue system manageable by everyone in team.
    Added vertical camera follow to player in Scene.

---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/Dialogue/DialogueManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    private Queue<string> sentences;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sentences = new Queue<string>();
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting Conversation with" + dialogue.name);
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if(sentences.Count != 0)
+            {
+                DisplayNextSentence();
+            }
+            else
+            {
+                EndDialogue();
+            }
+        }
+    }
+
+    public void DisplayNextSentence()
+    {
+        if(sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        Debug.Log(sentence);
+    }
+
+    void EndDialogue()
+    {
+        if(GetComponent<BoxCollider>().enabled)
+        {
+            Debug.Log("End of Convo");
+            GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+}
---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/Dialogue/DialogueTrigger.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+    public Dialogue dialogue;
+
+    public void TriggerDialogue()
+    {
+        GetComponent<DialogueManager>().StartDialogue(dialogue);
+    }
+
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // on object npc
+        if (gameObject.CompareTag("NPC"))
+        {
+            if(other.CompareTag("Player"))
+            {
+                // Interacting with Player
+                TriggerDialogue();
+            }
+        }
+    }
+}
