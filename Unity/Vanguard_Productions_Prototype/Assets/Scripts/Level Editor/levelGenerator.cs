1e39c4ace881b854fc24c93dd8fa6149d634a12f
David Eammano <david.eammano@yahoo.com.au>
2020-08-11 16:01:51 +1000

    Changed level editor onto the Z axis

--- Unity/Vanguard_Productions_Prototype/Assets/Scripts/Level Editor/levelGenerator.cs
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/Level Editor/levelGenerator.cs
@@ -36,7 +36,7 @@ public class levelGenerator : MonoBehaviour
         {
             if (colorMapping.color.Equals(pixelColor))
             {
-                Vector2 position = new Vector2(x, y);
+                Vector3 position = new Vector3(0, y, x);
                 Instantiate(colorMapping.prefab, position, Quaternion.identity, transform);
             }
         }
