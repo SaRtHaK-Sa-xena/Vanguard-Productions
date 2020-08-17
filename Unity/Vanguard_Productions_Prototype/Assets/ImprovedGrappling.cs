bbc8e30f59f6693fc0804bb0d6064e8c4544bae5
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-08-17 10:25:40 +1000

    Update FPS Counter
    Added FPS Script that allows the fps to be tracked.
    Attach to text and will display frames.

---
+++ Unity/Vanguard_Productions_Prototype/Assets/ImprovedGrappling.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImprovedGrappling : MonoBehaviour
+{
+    private LineRenderer lr;
+    private Vector3 grapplePoint;
+    public LayerMask grappable;
+    public Transform gunTip, player;
+    private float maxDistance = 1000f;
+    private SpringJoint joint;
+
+
+    private void Awake()
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            startGrapple();
+        }
+        if(Input.GetKeyUp(KeyCode.E))
+        {
+            stopGrapple();
+        }
+    }
+
+    private void LateUpdate()
+    {
+        DrawRope();
+    }
+
+    void startGrapple()
+    {
+        joint = player.gameObject.AddComponent<SpringJoint>();
+        joint.autoConfigureConnectedAnchor = false;
+        joint.connectedAnchor = grapplePoint;
+
+        float distanceFromPoint = Vector3.Distance(player.position, grapplePoint);
+
+        joint.maxDistance = distanceFromPoint * 0.0f;
+        joint.maxDistance = distanceFromPoint * 0.25f;
+
+        joint.spring = 4.5f;
+        joint.damper = 7f;
+        joint.massScale = 4.5f;
+
+        lr.positionCount = 2;
+    }
+
+
+    void stopGrapple()
+    {
+        lr.positionCount = 0;
+        Destroy(joint);
+    }
+
+    void DrawRope()
+    {
+        if (!joint)
+        {
+            return;
+        }
+        else
+        {
+            lr.SetPosition(0, gunTip.position);
+            lr.SetPosition(0, grapplePoint);
+        }
+    }
+}
