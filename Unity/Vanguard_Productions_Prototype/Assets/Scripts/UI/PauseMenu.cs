992f7aa4f6f7bb70dd6f7cc010357fc7cec82e61
BrokenPIG <david.eammano@yahoo.com.au>
2020-09-08 19:38:11 +1000

    Added PauseMenu

---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/UI/PauseMenu.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI; 
+
+    void Update() // CHeck if key is pressed to open/close Pause Menu
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume () // Pause menu Closed
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    public void Pause () // Pause menu Opened
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+
+    public void Menu (string SceneName)
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneName);
+    }
+
+    public void QuitGame ()
+    {
+        Application.Quit();
+    }
+}
