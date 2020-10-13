3930188c6b7154b3330e672464f5963978ed1238
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-10-13 16:12:05 +1100

    Update
    Saving Progress.

---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/Enemy/EnemyPatrol.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrol : MonoBehaviour
+{
+    public GameObject Enemy;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Enemy.GetComponent<EnemyMovement>().patrol = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Enemy.GetComponent<EnemyMovement>().patrol = false;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            Enemy.GetComponent<EnemyMovement>().patrol = true;
+        }
+    }
+
+}
