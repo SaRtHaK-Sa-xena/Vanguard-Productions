36c71a420ac41b5c819260de3af885e11c5e2e16
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-11-10 16:42:11 +1100

    Warden Animations
    Animations In Place for Warden.
    Need few tweakings to complete the attack sequence.

--- Unity/Vanguard_Productions_Prototype/Assets/Scripts/Enemy/Boss/wardenAI.cs
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/Enemy/Boss/wardenAI.cs
@@ -13,33 +13,54 @@ public class wardenAI : MonoBehaviour
 
     public Transform playerTarget;
 
-    public bool patrol;
+    public bool patrol, attackPlayer;
 
     public Transform[] patrolPoints;
     private int waypointIndex;
     private float dist;
 
+    // stunned variables
+    private bool stunned;
+
+    // attack variables
+    private float currentAttackTime;
+    private float defaultAttackTime = 1f;
+
+    private float currentTimeTrack = 0;
+    private float maxTime = 40f;
+
     // Start is called before the first frame update
     void Start()
     {
         patrol = true;
+        attackPlayer = false;
 
         myBody = GetComponent<Rigidbody>();
+        stunned = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(patrol)
+        if (patrol)
         {
             dist = Vector3.Distance(transform.position,
                 patrolPoints[waypointIndex].position);
 
-            if(dist < 4f)
+            if (dist < 4f)
             {
                 IncreaseIndex();
             }
             Patrol();
+        }
+        else
+        {
+            //currentTimeTrack++;
+            //if (currentTimeTrack >= maxTime)
+            //{
+            //    currentTimeTrack = 0;
+            //    patrol = true;
+            //}
         }
     }
 
@@ -67,6 +88,37 @@ public class wardenAI : MonoBehaviour
         {
             Debug.Log("move left");
             myBody.velocity = -transform.forward * speed;
+        }
+    }
+
+    void Attack()
+    {
+        myBody.velocity = Vector3.zero;
+
+        if (!attackPlayer || stunned)
+        {
+            // exit
+            return;
+        }
+
+        currentAttackTime += Time.deltaTime;
+
+        if (currentAttackTime > defaultAttackTime)
+        {
+            enemyAnim.EnemyAttack(0);
+
+            currentAttackTime = 0f;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            patrol = false;
+            //attackPlayer = true;
+            //Attack();
+            enemyAnim.EnemyAttack(Random.Range(0,3));
         }
     }
 }
