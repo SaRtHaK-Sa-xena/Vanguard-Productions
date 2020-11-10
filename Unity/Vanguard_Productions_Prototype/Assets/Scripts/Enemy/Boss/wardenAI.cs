e8498f83a0284e9c8dd071d2e1484a8a665250a5
Sarthak Saxena <34013565+SaRtHaK-Sa-xena@users.noreply.github.com>
2020-11-10 16:10:47 +1100

    Added Warden Movement Patrol
    Patrol for Warden added.
    ONLY moving left and right for now.

---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/Enemy/Boss/wardenAI.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wardenAI : MonoBehaviour
+{
+
+    // animation handler
+    public animationScript enemyAnim;
+
+    private Rigidbody myBody;
+    public float speed = 5f;
+
+    public Transform playerTarget;
+
+    public bool patrol;
+
+    public Transform[] patrolPoints;
+    private int waypointIndex;
+    private float dist;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        patrol = true;
+
+        myBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(patrol)
+        {
+            dist = Vector3.Distance(transform.position,
+                patrolPoints[waypointIndex].position);
+
+            if(dist < 4f)
+            {
+                IncreaseIndex();
+            }
+            Patrol();
+        }
+    }
+
+    void IncreaseIndex()
+    {
+        waypointIndex++;
+        if (waypointIndex >= patrolPoints.Length)
+        {
+            waypointIndex = 0;
+        }
+    }
+
+
+    void Patrol()
+    {
+        // if the target is to the left
+        if (patrolPoints[waypointIndex].position.z < transform.position.z)
+        {
+            Debug.Log("move right");
+            myBody.velocity = transform.forward * speed;
+        }
+        
+        // if the target is to the right
+        if(patrolPoints[waypointIndex].position.z > transform.position.z)
+        {
+            Debug.Log("move left");
+            myBody.velocity = -transform.forward * speed;
+        }
+    }
+}
---
+++ Unity/Vanguard_Productions_Prototype/Assets/Scripts/UI/HealthBar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    // player Obj
+    [SerializeField] private GameObject playerObj;
+
+    // health Image
+    [SerializeField] private Image healthImage;
+
+    // updating speed of health decrease
+    [SerializeField] private float updateSpeedSeconds = 0.2f;
+
+    private void Awake()
+    {
+        // pass in event
+        playerObj.GetComponent<HealthScript>().OnHealthPctChanged += HandleHealthChanged;
+    }
+
+    void HandleHealthChanged(float pct)
+    {
+        // change to percentage
+        // pass in float value
+        StartCoroutine(ChangeToPct(pct));
+    }
+
+
+    private IEnumerator ChangeToPct(float pct)
+    {
+        float preChangePct = healthImage.fillAmount;
+        float elapsed = 0f;
+
+        while(elapsed < updateSpeedSeconds)
+        {
+            elapsed += Time.deltaTime;
+            healthImage.fillAmount = Mathf.Lerp(preChangePct, pct, elapsed / updateSpeedSeconds);
+            yield return null;
+        }
+
+        healthImage.fillAmount = pct;
+        if(healthImage.fillAmount <= 0)
+        {
+            healthImage.fillAmount = 100f;
+        }
+    }
+}
